49c34716f53161da6312d41d8097f41713bfbc29
Minsoo Choo <minsoochoo0122@proton.me>
2025-08-24 15:41:30 -0400

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
-	import favicon from '$lib/assets/favicon.svg';
+	import faviconPng from '$lib/assets/favicon-96x96.png';
+	import faviconSvg from '$lib/assets/favicon.svg';
+	import faviconIco from '$lib/assets/favicon.ico';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
+	import siteWebmanifest from '$lib/assets/site.webmanifest';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href={faviconPng} sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href={faviconSvg} />
+	<link rel="shortcut icon" href={faviconIco} />
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<meta name="apple-mobile-web-app-title" content="Packages" />
 </svelte:head>
 
 <ModeWatcher />
